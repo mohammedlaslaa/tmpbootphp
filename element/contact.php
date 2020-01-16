baa3d4ccb92ce2ffad4ceb48c732b6995a98b41e
Laslaa <mohamed.laslaa@gmail.com>
2020-01-16 16:10:09 +0100

    coursphp16012020

---
+++ element/contact.php
@@ -0,0 +1,17 @@
+<form action="/action_page.php">
+  <div><label for="fname">Prénom</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name.."></div>
+
+  <div><label for="lname">Nom</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name.."></div>
+
+  <div><label for="country">Pays</label>
+    <select id="country" name="country">
+      <option value="france">France</option>
+      <option value="belgium">Belgique</option>
+      <option value="luxembourg">Luxembourg</option>
+    </select></div>
+
+  <div><label for="subject">Sujet</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." ></textarea></div>
+</form>
---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Blog Home - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/blog-home.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <?php
+
+
+  // Display the date
+  var_dump(getdate());
+
+  $date = getdate();
+
+  echo $date['year'];
+
+  echo "<br>";
+
+  // Set the default date
+
+  date_default_timezone_set('Europe/Paris');
+
+  $x = strtotime("-1week");
+
+  echo $x;
+
+  echo "<br>";
+
+  echo date("d-m-Y", $x);
+
+  echo "<br>";
+
+  echo date("Y-m-d H:i:s");
+
+  echo "<br>";
+
+  echo date("d-m-Y H:i:s") . "<br>";
+
+  echo time();
+
+  //Start session global variable
+
+
+  session_start();
+
+  $_SESSION["salut"] = "coucou";
+
+  /****** Read file ******/
+
+  require_once('element/nav.php');
+
+  // Test isset
+
+  $form = isset($_GET["contact"]) ? "form" : "";
+
+  // display the file depending the key value
+
+  if ($form == "form") {
+    require_once('element/contact.php');
+  } else {
+    require_once('element/main.php');
+  }
+
+  require_once('element/footer.php');
+
+  // Test an existing file
+
+  $x = file_exists('element/script.php');
+
+  if ($x) {
+    require_once('element/script.php');
+  }
+
+  // Open new file and write it
+
+  $myfile = fopen("newfile.php", "w") or die("Unable to open file!");
+  $txt = "<h1>New File !</h1>";
+  fwrite($myfile, $txt); // Then you can require it in this file
+
+  // all information of the server
+
+  //  var_dump($_SERVER);
+
+  function getMinDate($arg1, $arg2 = false)
+  {
+    $x = strtotime($arg1);
+    if (!$arg2) {
+      $y = time();
+    } else {
+      $y = strtotime($arg2);
+    }
+    if ($x > $y) {
+      return $arg1 . " est plus récent que " . date('d-m-y', $y) . " et son timestamp est " . $x;
+    } elseif ($y > $x) {
+      return date('d-m-y', $y) . " est plus récent que " . $arg1 . " et son timestamp est " . $y;
+    }
+  }
+
+  echo getMinDate('12-01-20');
+
+  function getNbDay($arg1, $arg2)
+  {
+    $datetime1 = date_create($arg1);
+    $datetime2 = date_create($arg2);
+    $interval = date_diff($datetime1, $datetime2);
+    return $interval->format('%R%a days');
+  }
+
+  echo getNbDay('19-01-2019', '20-01-2020');
+
+  function increase(&$x): void
+  {
+    $x *= 5;
+  }
+
+  $xiu = 100;
+
+  echo $xiu;
+  increase($xiu);
+  echo $xiu;
+
+  echo "<br>";
+
+  echo md5("hllkflkj");
+
+  echo "<br>";
+
+  echo password_hash("hekdkdkd", PASSWORD_DEFAULT);
+
+
+
+  ?>
+</body>
+
+</html>
